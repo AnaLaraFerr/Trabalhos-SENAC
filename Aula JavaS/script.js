784612a3cb0a1e9b464217605032491941ff0bc7
AnaLaraFerr <analaraferrsilv@gmail.com>
2026-04-28 15:48:55 -0300

    Add files via upload

---
+++ Aula JavaS/script.js
@@ -0,0 +1,187 @@
+function leticia(){
+    alert("Olá Mundo");
+    let nome = "Ana Lara";
+    alert(nome);
+    let numero = 17;
+    let numero2 = 9;
+    let numero3 = 1;
+    let resultado = numero + numero2+ numero3;
+    alert(numero +"+"+ numero2);
+    alert(resultado);
+}
+
+function mudar(){
+    let texto = document.getElementById("textinho");
+    texto.textContent = "Sla irmao";
+}
+
+function sabrina2(){
+    let cor = document.getElementById ("sabrina");
+    cor.style.backgroundColor = "red";
+}
+
+function rafaela(){
+    let clique = document.getElementById ("rafaella");
+    clique.textContent = "Voce clicou";
+}
+
+function gabriela(){
+    let apague = document.getElementById ("gabi");
+    apague.textContent = "";
+}
+
+function funcao(){
+    let par = document.getElementById("paragrafo");
+    let cax = document.getElementById("caixa");
+    let escrita = par.textContent;
+    cax.value = escrita;
+}
+
+function soma(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let resultado = Number(num1) + Number(num2);
+    let local = document.getElementById("resultado");
+    local.textContent = resultado;
+ }
+
+ function tudo(){
+    let mudar = document.getElementById ("tudao");
+    mudar.style.backgroundColor = "red";
+ }
+
+ function tamanho(){
+    let fontezona = document.getElementById ("imensa");
+    fontezona.style.fontSize = "30px";
+ }
+
+ function fontecor(){
+    let transs = document.getElementById ("trans");
+    transs.style.color = "blue";
+ }
+
+ function doisaomesmo(){
+    let mesmo = document.getElementById ("tchu1");  
+    mesmo.textContent = "MUDOU!";
+    let mesmo2 = document.getElementById ("tchu");
+    mesmo2.textContent = "MUDOU!";
+ }
+
+ function tresaomesmo(){
+    let mesma = document.getElementById ("lulu");
+    mesma.textContent = "MUDOU!";
+    let mesma2 = document.getElementById ("lulu2");
+    mesma2.textContent = "MUDOU!";
+    let mesma3 = document.getElementById ("lulu3");
+    mesma3.textContent = "MUDOU!";
+ }
+
+ function trensao(){
+    let mudartexto = document.getElementById ("trem");
+    mudartexto.textContent = "Voce clicou";
+    let cordele = document.getElementById ("trem");
+    cordele.style.color = "green";
+    let tamanhodele = document.getElementById ("trem");
+    tamanhodele.style.fontSize = "30px";
+ }
+
+ function juliah(){
+    let porque = document.getElementById ("julia");
+    porque.textContent = "Voce clicou";
+    let voce = document.getElementById ("julia");
+    voce.style.color = "green";
+    let nao = document.getElementById ("julia");
+    nao.style.fontSize = "30px";
+
+    let porque2 = document.getElementById ("julia2");
+    porque2.textContent = "Voce clicou";
+    let voce2 = document.getElementById ("julia2");
+    voce2.style.color = "green";
+    let nao2 = document.getElementById ("julia2");
+    nao2.style.fontSize = "30px";
+
+ }
+
+ function gigi(){
+    let tavares = document.getElementById ("giovana");
+    tavares.textContent = "Voce clicou";
+    let tavares2 = document.getElementById ("giovana");
+    tavares2.style.color = "green";
+    let tavares3 = document.getElementById ("giovana");
+    tavares3.style.fontSize = "30px";
+
+    let carmo = document.getElementById ("giovana2");
+    carmo.textContent = "Voce clicou";
+    let carmo2 = document.getElementById ("giovana2");
+    carmo2.style.color = "green";
+    let carmo3 = document.getElementById ("giovana2");
+    carmo3.style.fontSize = "30px";
+
+    let lima = document.getElementById ("giovana3");
+    lima.textContent = "Voce clicou";
+    let lima2 = document.getElementById ("giovana3");
+    lima2.style.color = "green";
+    let lima3 = document.getElementById ("giovana3");
+    lima3.style.fontSize = "30px";
+
+ }
+
+ function zaquin(){
+    let chato = document.getElementById("paragrafo2");
+    let chato2 = document.getElementById("isac2");
+    let chato3 = document.getElementById("isac3");
+    let escrita = chato.textContent;
+    chato2.value = escrita;
+    chato3.value = escrita;
+ }
+
+ function alice(){
+    let lice = document.getElementById("riti");
+    lice.textContent = "Mudou!";
+ }
+
+ function marcus(){
+    let marquin = document.getElementById("riti");
+    marquin.style.color = "green";
+ }
+
+ function tata(){
+    let tauanny = document.getElementById("riti");
+    tauanny.style.fontSize = "30px";
+ }
+
+ function mudardiv()
+ {
+    let alvodacor = document.getElementById("div1");
+    alvodacor.style.backgroundColor="red";
+
+    let alvodacor2 = document.getElementById("div2");
+    alvodacor2.style.backgroundColor="red";
+ }
+
+ function mael(){
+    let maell = document.getElementById("emanuel").value;
+    let local = document.getElementById("md");
+    local.textContent = maell;
+    local.style.color = "green";
+ }
+
+ function sophia(){
+    let pene = document.getElementById("soso").value;
+    let lope = document.getElementById("soso2").value;
+    let resultado2 = Number(pene) + Number(lope);
+    let local2 = document.getElementById("resultado2");
+    local2.textContent = resultado2;
+ }
+
+ function sophia2(){
+    let vazio = document.getElementById("resultado2");
+    vazio.textContent = ""
+ }
+
+ function maria(){
+    let mudar = document.getElementById ("tudao");
+    mudar.style.backgroundColor = ("red");
+    mudar.style.fontSize = ("20px")
+    mudar.style.color = ("white")
+ }
