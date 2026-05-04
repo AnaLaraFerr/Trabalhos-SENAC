24381ed65ec2ff7e4423414c23ea7e87f9a1a1a4
AnaLaraFerr <analaraferrsilv@gmail.com>
2026-05-04 16:38:00 -0300

    Add files via upload

---
+++ AulaJavaS(2)/script.js
@@ -0,0 +1,196 @@
+let numero = 10;
+if(numero ==10);     
+{
+    alert("O numero é 10");
+}
+
+
+let nome = "ana";
+if (nome == "ana");
+{
+    alert("Meu nome é ana!");
+}
+
+function primeiro(){
+
+    let Ana = document.getElementById("num1").value;
+    if (Ana == "ok") 
+    {
+        alert("Correto");
+    }
+}
+
+function segundo(){
+
+    let sla = document.getElementById("num2").value;
+    if (sla ==5)
+    {
+            alert("Correto");
+    }
+}
+
+function terceiro(){
+
+    let mudar = document.getElementById("num3").value;
+    if (mudar =="admin")
+    {
+            document.getElementById("zaqui").textContent = "Bem-vindo";
+    }
+}
+
+function quarto(){
+
+    let mudar2 = document.getElementById("num4").value;
+    if (mudar2 =="teste")
+    {
+            document.getElementById("marquin").textContent = "Bem-vindo";
+    }
+}
+
+function quinto(){
+
+    let cor = document.getElementById("num5").value;
+    if (cor =="azul")
+    {
+            document.getElementById("soso").style.backgroundColor = "blue";
+    }
+}
+
+function sexto(){
+
+    let maior = document.getElementById("num6").value;
+    if (maior >=10) 
+    {
+        alert("Correto");
+    }
+}
+
+function setimo(){
+
+    let maior3 = document.getElementById("num7").value;
+    if (maior3 <=50) 
+    {
+        alert("Correto");
+    }
+}
+
+function oitavo(){
+
+    let mudar2 = document.getElementById("num8").value;
+    if (mudar2 =="senha")
+    {
+            document.getElementById("gabi").textContent = "Será msm?";
+    }
+}
+
+function nono(){
+
+    let cor2 = document.getElementById("num9").value;
+    if (cor2 =="escuro")
+    {
+            document.getElementById("tudo").style.backgroundColor = "black";
+    }
+}
+
+function decimo(){
+
+    let mais = document.getElementById("num10").value;
+    if (mais == "analinda")
+    {
+            document.getElementById("lulu").textContent = "Mudou!";
+            document.getElementById("lulu").style.backgroundColor = "Red";
+    }
+}
+
+function decimoprime(){
+
+    let tchu = document.getElementById("num11").value;
+    if (tchu == "ok")
+    {
+            document.getElementById("lulu").style.fontSize = "30px";
+            document.getElementById("tudo").style.backgroundColor = "Red";
+    }
+}
+
+function decimoseg(){
+
+    let fonte = document.getElementById("num12").value;
+    if (fonte >=20) 
+    {
+         document.getElementById("alice").style.fontSize = "30px";
+    }
+}
+
+function decimoter(){
+
+    let fds = document.getElementById("num13").value;
+    if (fds =="admin")
+    {
+            alert("Bem-Vindo")
+            document.getElementById("penelope").textContent = "Bem-vindo";
+    }
+}
+
+function decimoquar(){
+
+    let tchu2 = document.getElementById("num14").value;
+    if (tchu2 ==27)
+    {
+            document.getElementById("maria").style.fontSize = "40px";
+            document.getElementById("maria").textContent = "Sertanejo e legal";
+            document.getElementById("maria").style.color = "blue";
+    }
+}
+
+function decimoquin(){
+
+    let foudasse = document.getElementById("num15").value;
+    if (foudasse == "anaperfeita")
+    {
+            document.getElementById("tudo").style.fontSize = "40px";
+            document.getElementById("tudo").style.backgroundColor = "pink";
+            document.getElementById("tudo").style.color = "blue";
+    }
+}
+
+function decimosex(){
+
+    let cor = document.getElementById("num16").value;
+    if (cor >=100)
+    {
+            document.getElementById("tudo").style.backgroundColor = "purple";
+    }
+}
+
+function decimoset(){
+
+    let paiva = document.getElementById("num17").value;
+    if (paiva =="copiar")
+    {
+            document.getElementById("paivaa").textContent = "Copiar!";
+    }
+}
+
+function decimooit(){
+
+    let malev = document.getElementById("num18").value;
+    if (malev ==5)
+    {
+            document.getElementById("malevola").style.backgroundColor = "blue";
+            document.getElementById("malevola2").style.backgroundColor = "blue";
+    }
+}
+
+function decimonono(){
+
+    let isso = document.getElementById("num19").value;
+    if (isso ==25)
+    {
+            document.getElementById("tudo").style.fontSize = "40px";
+            document.getElementById("tudo").style.backgroundColor = "pink";
+            document.getElementById("tudo").style.color = "blue";
+    }
+}
+
+
+
